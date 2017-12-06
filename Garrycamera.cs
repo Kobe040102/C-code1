d99313b40e83685d25506fd7e7297f9fd462a034
kobe040102 <kobeshelby04@gmail.com>
2017-12-05 20:12:34 -0800

    C# code

---
+++ Garrycamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Garrycamera : MonoBehaviour
+{
+    public Transform player;
+
+    public bool bound;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos; 
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update ()
+    {
+        transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
+
+        if (bound)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+                
+        }
+    }
+
+}
